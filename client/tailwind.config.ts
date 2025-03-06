385b6d08f50122494dc8e97a9a6d8556931b6705
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-03-06 19:46:44 +0800

    Add constants and validation schemas for property and application forms

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 
-export default {
-    darkMode: ["class"],
-    content: [
+const config: Config = {
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -21,12 +21,30 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				'50': '#fcfcfc',
+  				'100': '#f1f1f2',
+  				'200': '#e0e0e2',
+  				'300': '#c7c7cc',
+  				'400': '#a8a8af',
+  				'500': '#82828b',
+  				'600': '#57575f',
+  				'700': '#27272a',
+  				'800': '#111113',
+  				'900': '#040405',
+  				'950': '#000000'
   			},
   			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
+  				'50': '#fefcfc',
+  				'100': '#fdf2f2',
+  				'200': '#fae1e1',
+  				'300': '#f6c9c9',
+  				'400': '#f1abab',
+  				'500': '#eb8686',
+  				'600': '#e45a5a',
+  				'700': '#dc2828',
+  				'800': '#7c1414',
+  				'900': '#400a0a',
+  				'950': '#2c0707'
   			},
   			muted: {
   				DEFAULT: 'hsl(var(--muted))',
@@ -60,13 +78,9 @@ export default {
   				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
   			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
   		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
+export default config;
